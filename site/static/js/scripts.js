cb6331dc846ccc9813b1af107b20bc87bf304733
ali <wizard2070@gmail.com>
2023-02-27 22:53:24 +0330

    minor_improvement

--- site/static/js/scripts.js
+++ site/static/js/scripts.js
@@ -3,7 +3,7 @@ jQuery(document).ready(function ($) {
     // Raspi Thumnail on mobile size 
     const raspiThumbnailMobile = $('#raspi_thumbnail_mobile');
     const raspiThumbnailDesktop = $('#raspi_thumbnail_desktop');
-    if (window.matchMedia('(max-width: 700px)').matches) {
+    if (window.matchMedia('(max-width: 768px)').matches) {
         $(raspiThumbnailDesktop).remove();
         $(raspiThumbnailMobile).show('fast');
     } else {
